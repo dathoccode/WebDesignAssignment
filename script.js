259acb66f9b32c69ebf76b824cee62d949170634
Ducutc <ducutc22@gmail.com>
2024-10-27 01:42:21 +0700

    copy

--- script.js
+++ script.js
@@ -7,41 +7,47 @@ const hotSaleProduct = document.querySelectorAll('.hot-sale-product-container')
 
 let index = 0
 hotSaleProduct.forEach(items =>{
-    items.style.left = 20 * index + "%"
+    items.style.left = 20 * index + "%" 
     index++
 })
-
 index = 0
 btnLeft.addEventListener("click", function(){
-    if(index <= 0) return
+    if(index <= 0) 
+    {
+        btnLeft.style.display = "none"
+        return
+    }
+
     hotSaleProduct.forEach(items => {
         const curLeft = parseFloat(window.getComputedStyle(items).left)
-        const newLeft = curLeft + 0.2 *  containerWidth + 0.1
+        const newLeft = curLeft + 0.2 *  containerWidth 
         items.style.left = newLeft + "px"
-        console.log(curLeft + " " + newLeft)
     })
     index--
 })
 btnRight.addEventListener("click", function(){
-    if(index > hotSaleProduct.length - 6) return
+    if(index > hotSaleProduct.length - 6)
+    {
+        btnRight.style.display = "none"
+        return
+    }
     hotSaleProduct.forEach(items => {
         const curLeft = parseFloat(window.getComputedStyle(items).left)
-        const newLeft = curLeft - 0.2 *  containerWidth + 0.1
+        const newLeft = curLeft - 0.2 *  containerWidth
         items.style.left = newLeft + "px"
-        console.log(curLeft + " " + newLeft)
     })
     index++
 })
 function hotSaleAuto()
 {
-    if(index >= hotSaleProduct.length - 6)
+    if(index > hotSaleProduct.length - 6 || index < 0)
     {
         index = 0
         hotSaleProduct.forEach(items =>{
             items.style.left = 20 * index + "%"
             index++
         })
-        index = 0;
+        index = 0
     }
     else
     {
@@ -49,9 +55,17 @@ function hotSaleAuto()
             const curLeft = parseFloat(window.getComputedStyle(items).left)
             const newLeft = curLeft - 0.2 *  containerWidth + 0.1
             items.style.left = newLeft + "px"
-            console.log(curLeft + " " + newLeft)
         })
+        index++
     }
+    btnLeft.style.display = "block"
+    btnRight.style.display = "block"
 }
 
 setInterval(hotSaleAuto, 5000)
+
+// ---------------------Reload-----------------------------------
+const brandLogo = document.querySelector(".brand-logo")
+brandLogo.addEventListener("click", function(){
+    location.reload()
+})
